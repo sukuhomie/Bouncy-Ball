03b475c8049d084336485d97f4ec8962af96ad51
sukuhomie <ppnoob35@gmail.com>
2026-09-17 21:35:03 +0200

    First build
    got the jump and the pipes

---
+++ Assets/scripts/MovePipeScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MovePipeScript : MonoBehaviour
+{
+
+    public float moveSpeed = 5;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + (Vector3.left * moveSpeed);
+    }
+}
