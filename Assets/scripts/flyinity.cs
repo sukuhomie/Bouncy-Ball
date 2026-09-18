8d83a29d2a30989a3f0234592d0392afce1ce0c7
sukuhomie <ppnoob35@gmail.com>
2026-09-18 20:21:24 +0200

    FINISHED THE FLAPPY BIRD THINGY
    need to build it and push it to main but im tryna add sound or stuff like that

--- Assets/scripts/flyinity.cs
+++ Assets/scripts/flyinity.cs
@@ -3,20 +3,27 @@ using UnityEngine;
 public class flyinity : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
-    public float flapStrength; 
+    public float flapStrength;
+    public LogicScript logic;
+    public bool BirdIsAlive = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true && BirdIsAlive == true)
         {
             myRigidBody.linearVelocity = Vector2.up * flapStrength;
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        BirdIsAlive = false;
     }
 }
